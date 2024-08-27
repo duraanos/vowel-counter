02152680960dbddfdea5534b04c6b251a8ffea39
duraanos <duran364@anadolu.edu.tr>
2024-08-27 13:17:29 +0300

    Import files from config.js

--- src/app.js
+++ src/app.js
@@ -1,25 +1,24 @@
-'use strict';
+import { TEXTAREA, VOWELS_ARR, PUNCTUATIONS } from './config.js';
 
 const vowels = document.querySelector('.vowels-count');
 const consonants = document.querySelector('.consonants-count');
 
-
 const countChars = function () {
-  const text = textarea.value;
-  const newText = text.replace(' ', '').replace(punctuations, '');
+  const text = TEXTAREA.value;
+  const newText = text.replace(' ', '').replace(PUNCTUATIONS, '');
   let numVowels = 0;
   let numConsonants = 0;
 
   [...newText.toLowerCase()]
     .filter((value, index, self) =>
-      vowelsArr.includes(value) ? value : self.indexOf(value) === index
+      VOWELS_ARR.includes(value) ? value : self.indexOf(value) === index
     )
     .forEach(char =>
-      vowelsArr.includes(char) ? numVowels++ : numConsonants++
+      VOWELS_ARR.includes(char) ? numVowels++ : numConsonants++
     );
 
   vowels.textContent = numVowels;
   consonants.textContent = numConsonants;
 };
 
-textarea.addEventListener('input', countChars);
+TEXTAREA.addEventListener('input', countChars);
