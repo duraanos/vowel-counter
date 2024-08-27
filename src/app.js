d9968019c60c53e065b3026db74aa5cefad6beb9
duraanos <duran364@anadolu.edu.tr>
2024-08-27 14:10:06 +0300

    Remove the vowel counter function from app.js

--- src/app.js
+++ src/app.js
@@ -25,32 +25,5 @@ const countChars = function () {
   consonants.textContent = numConsonants;
 };
 
-const countVowels = function () {
-  const numA = document.querySelector('.a-count');
-  const numE = document.querySelector('.e-count');
-  const numI = document.querySelector('.i-count');
-  const numO = document.querySelector('.o-count');
-  const numU = document.querySelector('.u-count');
-
-  const text = textarea.value;
-  const newText = text.replace(' ', '').replace(punctuations, '');
-
-  const eachVowels = [...newText.toLowerCase()].reduce(
-    (acc, char) => {
-      if (vowelsArr.includes(char)) acc[char] = (acc[char] || 0) + 1;
-      return acc;
-    },
-    { a: 0, e: 0, i: 0, o: 0, u: 0 }
-  );
-
-  const vowelsObj = eachVowels;
-
-  numA.textContent = vowelsObj.a;
-  numE.textContent = vowelsObj.e;
-  numI.textContent = vowelsObj.i;
-  numO.textContent = vowelsObj.o;
-  numU.textContent = vowelsObj.u;
-};
-
 textarea.addEventListener('input', countChars);
 textarea.addEventListener('input', countVowels);
