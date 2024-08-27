4c5ef94037ce96d7bd14e9f7ddd303f8282cfe70
duraanos <duran364@anadolu.edu.tr>
2024-08-27 21:39:54 +0300

    Change directory of js files

--- src/config.js
+++
@@ -1,3 +0,0 @@
-export const TEXTAREA = document.querySelector('textarea');
-export const VOWELS_ARR = ['a', 'e', 'i', 'o', 'u'];
-export const PUNCTUATIONS = /[.,?\/#!$%\^&\*;:{}=\-_`~()]/g;
--- src/helpers.js
+++
@@ -1,6 +0,0 @@
-import { TEXTAREA, PUNCTUATIONS } from './config.js';
-
-export const generateText = function () {
-  const text = TEXTAREA.value;
-  return text.toLowerCase().replace(' ', '').replace(PUNCTUATIONS, '');
-};
