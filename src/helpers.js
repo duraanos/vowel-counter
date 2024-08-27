be06b79c9aa9724c15d219cbd2a51a91bd8cf3d0
duraanos <duran364@anadolu.edu.tr>
2024-08-27 14:00:56 +0300

    Dry the generateText function

--- src/helpers.js
+++ src/helpers.js
@@ -2,6 +2,5 @@ import { TEXTAREA, PUNCTUATIONS } from './config.js';
 
 export const generateText = function () {
   const text = TEXTAREA.value;
-  const newText = text.toLowerCase().replace(' ', '').replace(PUNCTUATIONS, '');
-  return newText;
+  return text.toLowerCase().replace(' ', '').replace(PUNCTUATIONS, '');
 };
